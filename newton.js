5cff086aff2a5a983124705ac7ac2bb6803a6925
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2019-07-11 14:47:40 +0700

    add newton's method in JS

---
+++ newton.js
@@ -0,0 +1,28 @@
+const f = function (x) {
+    const value = 8-(4.5*(x-Math.sin(x)))
+    return value
+}
+
+const p = function(x){
+    const value = -4.5*(1-Math.cos(x))
+    return value
+}
+
+
+let x = [];
+x.push(2);
+
+let i;
+
+for(i=0;i<2;i++){
+
+    const temp = x[i] - (f(x[i]) / p(x[i]))
+    x.push(temp)
+    
+}
+
+
+console.log(x)
+
+
+
