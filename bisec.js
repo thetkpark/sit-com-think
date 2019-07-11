b856751260989a5bd7a43df8f6d07bf9916bd8d7
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2019-07-11 14:38:45 +0700

    add bisection method in JS

---
+++ bisec.js
@@ -0,0 +1,28 @@
+const f = function (x) {
+    const value = 8-(4.5*(x-Math.sin(x)))
+    return value
+}
+
+const calXNS = function(x,y) {
+    return (x+y)/2
+}
+
+let a=2,b=3;
+let xns = calXNS(a,b)
+let i=0;
+if(f(a)*f(b)<0){
+    for(i=0;i<10;i++){
+        if(f(a)*f(xns) < 0){
+            b = xns;
+            xns = calXNS(a,b)
+        }
+        else if(f(a)*f(xns)>0){
+            a = xns
+            xns = calXNS(a,b)
+        }
+        
+    
+    }
+}
+
+console.log(xns);
